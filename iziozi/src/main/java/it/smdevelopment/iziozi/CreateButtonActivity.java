2bab137c334f5610b508a628d73a9b7aeaafdd89
Martino Lessio <martino.lessio@gmail.com>
2014-04-04 01:45:11 +0200

    minor fixes + create layout

--- iziozi/src/main/java/it/smdevelopment/iziozi/CreateButtonActivity.java
+++ iziozi/src/main/java/it/smdevelopment/iziozi/CreateButtonActivity.java
@@ -2,6 +2,8 @@ package it.smdevelopment.iziozi;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class CreateButtonActivity extends Activity {
 
@@ -15,5 +17,9 @@ public class CreateButtonActivity extends Activity {
 		
 		
 	}
+
+    public void doSave(View v){
+        Toast.makeText(getApplicationContext(),"save!",Toast.LENGTH_SHORT).show();
+    }
 
 }
